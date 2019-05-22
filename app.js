fa2a621206c901910fd5e590b3e76566a5322b21
adnanrahic <ado.raha198@gmail.com>
2019-05-22 14:11:48 +0200

    added sta to docker-compose/swarm

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', async (req, res, next) => {
+  console.log('FOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!')
   res.status(200).send('Hello World!')
 })
 app.listen(3000, () => console.log('Server is running on port 3000'))
