3e308bd581448e13199926da47de481ea5395c90
Adnan Rahic <ado.raha198@gmail.com>
2020-03-13 11:30:51 +0100

    added app

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ stMonitor.start()
 
 const express = require('express')
 const app = express()
+
+// setInterval(() => console.log('Tick Tock'), 3000)
+
 app.get('/', (req, res, next) => {
   console.log('Hello World!')
   res.status(200).send('Hello World!')
 })
 
 module.exports = app
-
