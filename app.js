919712099fce7fc2da36b9aff35998400c8c18f7
Adnan Rahic <ado.raha198@gmail.com>
2020-03-13 13:41:53 +0100

    change app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ stMonitor.start()
 const express = require('express')
 const app = express()
 
-// setInterval(() => console.log('Tick Tock'), 3000)
+setInterval(() => console.log('Tick Tock'), 3000)
 
 app.get('/', (req, res, next) => {
   console.log('Hello World!')
