6164fafe2a25c25d6b019d75a9c60f9648fdc580
Adnan Rahic <ado.raha198@gmail.com>
2020-04-06 17:11:43 +0200

    test logs_enabled refactor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Load env vars
 require('dotenv').config()
 // require stMonitor agent
-const { stMonitor, stLogger } = require('sematext-agent-express')
+// const { stMonitor, stLogger } = require('sematext-agent-express')
 // Start monitoring metrics
-stMonitor.start()
+// stMonitor.start()
 
 const express = require('express')
 const app = express()
@@ -18,22 +18,37 @@ const errStack = `TypeError: Cannot create property 'Symbol(level)' on string 'T
   at listOnTimeout (timers.js:263:5)
   at Timer.processTimers (timers.js:223:10)`
 
-// setInterval(() => stLogger.info('Tick Tock'), 3000)
+setInterval(() => {
+  console.info(`
+    Tick Tock
+    LOGS_ENABLED_DEFAULT=true
+    LOGS_ENABLED=true/false
+  `)
+}, 3000)
 
 app.get('/', (req, res, next) => {
   // stLogger.info('Hello World.')
   // stLogger.debug('Hello debug.')
   // stLogger.warn('Some warning.')
   // stLogger.error(err)
-  // console.dir(err)
-  throw err
+  // throw err
+  console.log('inside /')
   res.status(200).send('Hello World.')
 })
 
-function errorHandler (err, req, res, next) {
-  stLogger.error(err)
-  console.log('inside errorHandler')
+app.get('/err', (req, res, next) => {
+  // stLogger.info('Hello World.')
+  // stLogger.debug('Hello debug.')
+  // stLogger.warn('Some warning.')
+  // stLogger.error(err)
+  // console.dir(err)
+  throw err
+  // res.status(200).send('Hello World.')
+})
 
+function errorHandler (err, req, res, next) {
+  console.error(err)
+  console.log('inside errorHandler')
   res.status(err.status || 500).send(err.message)
 }
 
