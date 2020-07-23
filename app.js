35030bbd2612cdbb7c88f62640e45f3338969dcb
Adnan Rahic <ado.raha198@gmail.com>
2020-07-23 17:10:58 +0200

    added a bunch of stuff

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Load env vars
 require('dotenv').config()
 // require stMonitor agent
-const { stMonitor } = require('sematext-agent-express')
-// Start monitoring metrics
-stMonitor.start()
+const { stHttpLoggerMiddleware } = require('sematext-agent-express')
 
 const express = require('express')
 const app = express()
+
+app.use(stHttpLoggerMiddleware)
+
 app.get('/', (req, res, next) => {
   console.log('Hello World!')
   res.status(200).send('Hello World!')
